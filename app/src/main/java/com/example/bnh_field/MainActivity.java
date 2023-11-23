3906d7db2a90e2133d2491f2c6b95f9fdf5b323f
asaduzzaman shahin <ashahin1995@gmail.com>
2023-11-24 00:02:09 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bnh_field/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bnh_field;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+import com.google.android.material.button.MaterialButton;
+
+
+
+public class MainActivity extends AppCompatActivity  /*implements View.OnClickListener*/ {
+   /* private Button login;
+    private Button forgot;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*TextView username = (TextView) findViewById(R.id.usernameID);
+        TextView password = findViewById(R.id.passID);
+        MaterialButton loginBtn = findViewById(R.id.logbtnID);
+        TextView forgotpass = findViewById(R.id.forgotID);*/
+
+
+    }
+
+   /* @Override
+    public void onClick(View v) {
+        Intent login=new Intent(this,home.class); startActivity(login);
+*/
+
+    }
---
+++ app/src/main/java/com/example/bnh_field/home.java
@@ -0,0 +1,4 @@
+package com.example.bnh_field;
+
+public class home {
+}
